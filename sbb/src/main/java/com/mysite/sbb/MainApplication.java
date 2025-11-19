b19b5fc4fd5d5a822e953d683e9c67937edcdaf9
kimhss <hyunuary108@kyonggi.ac.kr>
2025-11-19 15:07:15 +0900

    JSB - 2-02 간단한 웹 프로그램 만들기

---
+++ sbb/src/main/java/com/mysite/sbb/MainApplication.java
@@ -0,0 +1,17 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainApplication {
+    @GetMapping("/")
+    public void index() {
+        System.out.println("index() 호출");
+    }
+
+    @GetMapping("hello")
+    public void hello() {
+        System.out.println("hello() 호출");
+    }
+}
